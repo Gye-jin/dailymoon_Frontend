d3f6d96deb8a6d1424a4fb33a6768fe741fe6839
Hyunio <hjizz1686@gmail.com>
2023-02-08 00:57:55 +0900

    add kakao_Login_Api

---
+++ src/Api/KakaoLoginData.js
@@ -0,0 +1,2 @@
+export const REST_API_KEY = "95162824386174024377767fd9449424";
+export const REDIRECT_URI = "http://localhost:3000/api/login";
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,26 +1,33 @@
 import React from "react";
 import "../App.css";
 import Header from "../components/Header";
+import { REST_API_KEY, REDIRECT_URI } from "../Api/KakaoLoginData";
 
 function Login() {
+  const KakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}`;
+
+  const handleLogin = () => {
+    window.location.href = KakaoAuthUrl;
+  };
+
   return (
     <div className="background">
-      {/* <Header /> */}
+      <Header />
       <div className="login-button_wrapper">
         <img
           src="/img/kakao_login_small.png"
           className="kakao_login_small"
-          onClick="/api/diarys"
+          onClick={handleLogin}
         />
         <img
           src="/img/kakao_login_medium.png"
           className="kakao_login_medium"
-          onClick="/api/diarys"
+          onClick={handleLogin}
         />
         <img
           src="/img/kakao_login_large.png"
           className="kakao_login_large"
-          onClick="/api/diarys"
+          onClick={handleLogin}
         />
       </div>
     </div>
