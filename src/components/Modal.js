7958b7bc782cb80227a2697a36f1cf4274956a99
Hyunio <hjizz1686@gmail.com>
2023-02-18 02:13:36 +0900

    add diaryread api

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,7 +1,8 @@
 import "../App.css";
-import { format } from "date-fns";
+import ReadDiaryData from "../Api/DiaryData";
+// import { format } from "date-fns";
 
-function Modal({ setModalOpen, selectedDate }) {
+function Modal_read({ setModalOpen, date }) {
   // 모달 끄기
   const closeModal = () => {
     setModalOpen(false);
@@ -13,9 +14,11 @@ function Modal({ setModalOpen, selectedDate }) {
         X
       </button>
       <p>모달창입니다.</p>
-      <h3>{selectedDate.toDateString()}</h3>
+      <h3 className="SelectedDate">{date}</h3>
+
+      {/* <ReadDiaryData /> */}
     </div>
   );
 }
 
-export default Modal;
+export default Modal_read;
--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -5,24 +5,32 @@ import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import Modal from "../components/Modal";
 import moment from "moment/moment";
-import { parse } from "date-fns";
+// import { parse } from "date-fns";
 
 function Diary() {
-  const [selectedDate, setSelectedDate] = useState(new Date());
-  const [ModalOpen, setModalOpen] = useState(false);
+  // const [selectedDate, setSelectedDate] = useState("");
   const [date, setDate] = useState(new Date());
+  const [ModalOpen, setModalOpen] = useState(false);
+
   const handleReadDiary = (e) => {
-    const date = new Date(e.target.getAttribute("data-date"));
-    if (isNaN(date)) {
-      console.error("Invalid date selected");
-      return;
-    }
-    setSelectedDate(date);
+    // const date = new Date(e.target.getAttribute("date"));
+    // const date = selectedDate;
+    // if (isNaN(date)) {
+    //   console.error("Invalid date selected");
+    //   return;
+    // }
+    // setSelectedDate(date);
+    // const json = JSON.stringify(date);
     setModalOpen(true);
   };
+  // console.log(selectedDate); // 오늘날짜나옴
+  console.log(date.toDateString()); // 클릭한 날짜나옴
+  // -- 모달에{date.toDateString()} 출력하면 화면날아감--
+  // -- 이걸 변수에 저장해서 보내야하나-> 그래도 toString을 못받아줌
+  console.log(typeof date);
 
-  console.log(date.toDateString());
-  console.log(date.type);
+  //백에서 받아온 표정데이터--- 종류별로 5가지 배열 만들기
+  const arr_smileDate = ["2023-02-11", "2023-02-04"];
 
   return (
     <div className="background">
@@ -33,11 +41,39 @@ function Diary() {
           formatDay={(locale, date) => moment(date).format("DD")} //'일' 없이 숫자만 출력
           onChange={setDate}
           value={date}
+          tileContent={({ date }) => {
+            // 날짜 타일에 컨텐츠 추가하기 (html 태그)
+            // 추가할 html 태그를 변수 초기화
+            let html = [];
+            // 현재 날짜가 post 작성한(기분별 날짜배열) 날짜 배열(mark)에 있다면, dot div 추가
+            if ("2023-02-17" === moment(date).format("YYYY-MM-DD")) {
+              html.push(<div className="cal_1heart">🥰</div>);
+            } else if (
+              arr_smileDate.find((x) => x === moment(date).format("YYYY-MM-DD"))
+            ) {
+              html.push(<div className="cal_2smile">😊</div>);
+            } else if ("2023-02-19" === moment(date).format("YYYY-MM-DD")) {
+              html.push(<div className="cal_3soso">😐</div>);
+            } else if ("2023-02-12" === moment(date).format("YYYY-MM-DD")) {
+              html.push(<div className="cal_4bad">😞</div>);
+            } else if ("2023-02-15" === moment(date).format("YYYY-MM-DD")) {
+              html.push(<div className="cal_5cry">🥲</div>);
+            } else {
+            }
+            // 아래 (다른조건)에 따라 다른값주기
+            return (
+              <>
+                <div className="flex justify-center items-center absoluteDiv">
+                  {html}
+                </div>
+              </>
+            );
+          }}
         />
       </div>
       <p className="text-center">
         <span className="datecheck">
-          Selected Date :{" "}
+          선택된 날짜 :{" "}
           {`${date.getFullYear()}-${(date.getMonth() + 1)
             .toString()
             .padStart(2, "0")}-${date.getDate().toString().padStart(2, "0")}`}
@@ -49,9 +85,8 @@ function Diary() {
         <button className="readDiary_btn" onClick={handleReadDiary}>
           일기확인
         </button>
-        {ModalOpen && (
-          <Modal setModalOpen={setModalOpen} selectedDate={selectedDate} />
-        )}
+        {/* 버튼을 눌렀을때(true가 되었을때)=> modal 컴포넌트 렌더링 */}
+        {ModalOpen && <Modal setModalOpen={setModalOpen} />}
       </div>
       <div className="dailymoon_wrapper">
         <img
