5b2fad66c22277b93f8974e544e2aed95e9a7d49
Hyunio <hjizz1686@gmail.com>
2023-02-24 17:12:56 +0900

    Update: kakao login api

--- src/pages/Kakao.js
+++ src/pages/Kakao.js
@@ -1,12 +1,35 @@
-// 보류
+// // 보류
 // 로그인 완료 페이지
 import React, { useState, useEffect } from "react";
 import { HEADER_STRING, TOKEN_PREFIX } from "../Api/JwtProperties";
 import axios from "axios";
 import Header from "../components/Header";
 import GetJwtToken from "../Api/GetJwtToken";
+import Cookies from "js-cookie";
 
-// function Kakao() {
+function Kakao() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const code = urlParams.get("code");
+  console.log(code);
+
+  if (code) {
+    axios
+      .get("http://localhost:8080/api/kakao", { params: { code } })
+      .then((response) => {
+        console.log(response.headers);
+        let jwt = response.headers[HEADER_STRING];
+        Cookies.set("jwt", jwt, { expires: 0.0069 });
+        // sessionStorage.setItem("jwt", jwt);
+      });
+  }
+
+  return (
+    <>
+      <a href="/api/diarys">버튼</a>
+    </>
+  );
+}
+export default Kakao;
 //   const [jwtToken, setJwtToken] = useState("");
 
 //   useEffect(() => {
@@ -17,61 +40,80 @@ import GetJwtToken from "../Api/GetJwtToken";
 //   }, []);
 
 //   const handleLogin = () => {
-//     const jwt = GetJwtToken;
+//     const jwt = "YOUR_JWT_TOKEN";
 //     setJwtToken(jwt);
 //     sessionStorage.setItem("jwtToken", jwt);
 //   };
 
-// const [status, setStatus] = useState("Log Out...");
-
-// const logout = async () => {
-//   try {
-//     await axios.get("/kakaoLogout");
-//     setStatus("Success");
-//   } catch (error) {
-//     setStatus("Failed");
-//   }
-// };
-
-// const kauthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=45285e0960a657197d7e58601b2e8c97&redirect_uri=http://localhost:3000/api/kakao&response_type=code`;
-// const code = new URL(window.location.href).searchParams.get("kauthUrl");
-
-const onLogin = () => {
-  axios
-    .get("/api/kakao")
-    .then((response) => {
-      console.log(response.data);
-      const { accessToken } = response.data;
-
-      // API 요청하는 콜마다 헤더에 accessToken 담아 보내도록 설정
-      axios.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
-
-      // accessToken을 localStorage, cookie 등에 저장하지 않는다!
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-};
-
-//   return (
-//     <div className="background">
-//       <Header />
-//       <div className="kakao_ll">
-//         {jwtToken ? (
-//           <div>
-//             <h3>JWT Token: {jwtToken} 로그인이 완료되었습니다.</h3>
-//             <a href="/api/diarys">My Diary</a>
-//           </div>
-//         ) : (
-//           <button onClick={handleLogin}>Login</button>
-//         )}
-//       </div>
-//       <div>
-//         <button onClick={logout}>Logout</button>
-//         <p>{status}</p>
-//       </div>
-//     </div>
-//   );
+//   return <div></div>;
 // }
 
-export default onLogin;
+// export default Kakao;
+// //   const [jwtToken, setJwtToken] = useState("");
+
+// //   useEffect(() => {
+// //     const jwtFromSession = sessionStorage.getItem("jwtToken");
+// //     if (jwtFromSession) {
+// //       setJwtToken(jwtFromSession);
+// //     }
+// //   }, []);
+
+// //   const handleLogin = () => {
+// //     const jwt = GetJwtToken;
+// //     setJwtToken(jwt);
+// //     sessionStorage.setItem("jwtToken", jwt);
+// //   };
+
+// // const [status, setStatus] = useState("Log Out...");
+
+// // const logout = async () => {
+// //   try {
+// //     await axios.get("/kakaoLogout");
+// //     setStatus("Success");
+// //   } catch (error) {
+// //     setStatus("Failed");
+// //   }
+// // };
+
+// // const kauthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=45285e0960a657197d7e58601b2e8c97&redirect_uri=http://localhost:3000/api/kakao&response_type=code`;
+// // const code = new URL(window.location.href).searchParams.get("kauthUrl");
+
+// const onLogin = () => {
+//   axios
+//     .get("/api/kakao")
+//     .then((response) => {
+//       console.log(response.data);
+//       const { accessToken } = response.data;
+
+//       // API 요청하는 콜마다 헤더에 accessToken 담아 보내도록 설정
+//       axios.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+
+//       // accessToken을 localStorage, cookie 등에 저장하지 않는다!
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//     });
+// };
+
+// //   return (
+// //     <div className="background">
+// //       <Header />
+// //       <div className="kakao_ll">
+// //         {jwtToken ? (
+// //           <div>
+// //             <h3>JWT Token: {jwtToken} 로그인이 완료되었습니다.</h3>
+// //             <a href="/api/diarys">My Diary</a>
+// //           </div>
+// //         ) : (
+// //           <button onClick={handleLogin}>Login</button>
+// //         )}
+// //       </div>
+// //       <div>
+// //         <button onClick={logout}>Logout</button>
+// //         <p>{status}</p>
+// //       </div>
+// //     </div>
+// //   );
+// // }
+
+// export default onLogin;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,17 +1,35 @@
-import React from "react";
+import { useEffect } from "react";
 import "../App.css";
 import { HEADER_STRING, TOKEN_PREFIX } from "../Api/JwtProperties";
 import axios from "axios";
+import Cookies from "js-cookie";
 
 function Main() {
-  const kauthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=45285e0960a657197d7e58601b2e8c97&redirect_uri=http://localhost:3000/api/kakao&response_type=code`;
-  // KakaoLoginRedirect();
+  const handleKakaoLogin = () => {
+    const kauthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=45285e0960a657197d7e58601b2e8c97&redirect_uri=http://localhost:3000/api/kakao&response_type=code`;
+    window.location.href = kauthUrl;
+    console.log("로그인까지");
+  };
+  // function Main() {
+  //   const HandleKakaoLogin = () => {
+  //     //카카오 로그인 페이지로 이동
+  //     const kauthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=45285e0960a657197d7e58601b2e8c97&redirect_uri=http://localhost:3000/api/kakao&response_type=code`;
+  //     window.location.href = kauthUrl;
 
-  // axios.get("/api/kakao", { headers: { key: "login" } }).then((response) => {
-  //   var jwt = response.headers[HEADER_STRING];
-  //   console.log(jwt);
-  // 전달받은 JWT 를 처리하는 코드
-  // });
+  //     //인가코드 추출
+  //     const code = new URL(window.location.href).searchParams.get("code");
+  //     console.log(code);
+  //     if (code) {
+  //       axios
+  //         .get(`http://localhost:8080/api/kakao?code=${code}`)
+  //         .then((response) => {
+  //           console.log("success response"); // 백엔드로부터 받은 응답 처리
+  //         })
+  //         .catch((error) => {
+  //           console.log("error"); // 에러 처리
+  //         });
+  //     }
+  //   };
 
   return (
     <div className="background">
@@ -25,17 +43,17 @@ function Main() {
         <img
           src="/img/kakao_login_small.png"
           className="kakao_login_small"
-          onClick={() => (window.location.href = kauthUrl)}
+          onClick={handleKakaoLogin}
         />
         <img
           src="/img/kakao_login_medium.png"
           className="kakao_login_medium"
-          onClick={() => (window.location.href = kauthUrl)}
+          onClick={handleKakaoLogin}
         />
         <img
           src="/img/kakao_login_large.png"
           className="kakao_login_large"
-          onClick={() => (window.location.href = kauthUrl)}
+          onClick={handleKakaoLogin}
         />
       </div>
     </div>
@@ -43,3 +61,19 @@ function Main() {
 }
 
 export default Main;
+
+// axios
+//   .get("http://localhost:8080/api/kakao", { params: { code } })
+//   .then((response) => {
+//     const jwtToken = response.headers["authorization"];
+//     // console.log(code);
+//     // 쿠키 생성 및 저장
+//     Cookies.set("jwtToken", jwtToken, { expires: 0.0069 }); // 10분
+
+//     console.log("성공ㅇ");
+//   })
+//   .catch((error) => {
+//     console.log("로그인실패");
+//   });
+// })
+// };
